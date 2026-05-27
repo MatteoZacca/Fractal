048c7a044742962a470d235f820086f215f3c23f
MatteoZacca <zaccainter2001@gmail.com>
2026-05-27 15:30:19 +0200

    feat: implement NameNode entry point

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/MatteoZacca/distributed-file-system/internal/master"
+	"github.com/MatteoZacca/distributed-file-system/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	masterPort := ":9000"
+	metadataFile := "namespace.json"
+
+	leader := master.NewMetadataStore()
+
+	log.Printf("Booting NameNode... Attempting to load state from %s", metadataFile)
+	err := leader.LoadFromDisk(metadataFile)
+	if err != nil {
+		log.Fatalf("Critical Failure: Could not load metadata: %v", err)
+	}
+	log.Println("Metadata loaded successfully. System state restored.")
+
+	listener, err := net.Listen("tcp", masterPort)
+	if err != nil {
+		log.Fatalf("Failed to listen on %s: %v", masterPort, err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	nameNodeLogic := &master.NameNode{
+		Metadata: leader,
+	}
+	pb.RegisterMasterServiceServer(grpcServer, nameNodeLogic)
+
+	log.Printf("NameNode is ALIVE and listening on port %s", masterPort)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve gRPC: %v", err)
+	}
+}
