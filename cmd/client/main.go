6b1518887e68c15d8deab2bd0a4787df2ab9c60d
MatteoZacca <zaccainter2001@gmail.com>
2026-06-01 19:33:48 +0200

    feat: setup client-NameNode connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+
+	"github.com/MatteoZacca/distributed-file-system/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	masterConnection, err := grpc.NewClient("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to NameNode: %v", err)
+	}
+	defer masterConnection.Close()
+	masterClient := pb.NewMasterServiceClient(masterConnection)
+}
