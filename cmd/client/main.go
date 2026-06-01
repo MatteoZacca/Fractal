9fd4855f175a0947cfb1b0935290cdb28cf6603a
MatteoZacca <zaccainter2001@gmail.com>
2026-06-01 19:49:06 +0200

    feat: validate file and retrieve size information before upload

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/MatteoZacca/distributed-file-system/pb"
 	"google.golang.org/grpc"
@@ -9,6 +11,27 @@ import (
 )
 
 func main() {
+
+	if os.Args[1] != "upload" {
+		log.Fatalf("Type: upload <path-to-local-file>")
+	}
+
+	filePath := os.Args[2]
+	fileName := filepath.Base(filePath)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Could not open file %s: %v", filePath, err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatalf("Could not get file info: %v", err)
+	}
+	fileSize := fileInfo.Size()
+
+	log.Printf("Starting upload for '%s' (size: %d bytes)", fileName, fileSize)
 
 	masterConnection, err := grpc.NewClient("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
