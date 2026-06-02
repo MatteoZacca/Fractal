68c03f4239d6a4181361f076388d2e31614be420
MatteoZacca <zaccainter2001@gmail.com>
2026-06-02 11:09:17 +0200

    feat(client): loop through the blueprint after NameNode createFileResponse

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -34,6 +34,11 @@ func main() {
 
 	log.Printf("Starting upload for '%s' (size: %d bytes)", fileName, fileSize)
 
+	// ==========================================
+	// Talk to the NameNode
+	// ==========================================
+
+	// Connect to the NameNode
 	masterConnection, err := grpc.NewClient("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect to NameNode: %v", err)
@@ -41,6 +46,7 @@ func main() {
 	defer masterConnection.Close()
 	masterClient := pb.NewMasterServiceClient(masterConnection)
 
+	// Ask for the blueprint
 	log.Println("Requesting blueprint from NameNode...")
 	createFileResponse, err := masterClient.CreateFile(context.Background(), &pb.CreateFileRequest{
 		FilePath: fileName,
@@ -52,5 +58,28 @@ func main() {
 
 	chunksMapping := createFileResponse.ChunkLocations
 	log.Printf("Blueprint received! File split into %d chunks.", len(chunksMapping))
+
+	// ==========================================
+	// Talk to the DataNodes
+	// ==========================================
+
+	var chunkIDs []string
+
+	for chunkID, nodeList := range chunksMapping {
+		chunkIDs = append(chunkIDs, chunkID)
+
+		for _, dataNodeIP := range nodeList.WorkerIps {
+			log.Printf("Streaming %s to DataNode at %s...", chunkID, dataNodeIP)
+
+			err := uploadChunkToDataNode(file, chunkID, dataNodeIP)
+			if err != nil {
+				log.Fatalf("Failed to upload %s to %s: %v", chunkID, dataNodeIP, err)
+			}
+		}
+	}
+
+}
+
+func uploadChunkToDataNode(file *os.File, chunkID string, dataNodeIP string) error {
 
 }
